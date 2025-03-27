1b7f5fac4962d5cd34e30fd7017692cd71e47fe6
xadupre <xadupre@microsoft.com>
2025-03-27 10:29:29 +0100

    data

---
+++ _doc/api/torch_models/hghub/index.rst
@@ -0,0 +1,13 @@
+onnx_diagnostic.torch_models.hghub
+==================================
+
+.. toctree::
+    :maxdepth: 1
+    :caption: submodules
+
+    hub_api
+    hub_data
+
+.. automodule:: onnx_diagnostic.torch_models.hghub
+    :members:
+    :no-undoc-members:
--- onnx_diagnostic/torch_models/hghub/hub_api.py
+++ onnx_diagnostic/torch_models/hghub/hub_api.py
@@ -1,15 +1,53 @@
 from typing import List, Optional, Union
 import transformers
 from huggingface_hub import HfApi
+from .hub_data import __date__, load_architecture_task
 
 
-def get_task(model_id: str) -> str:
+def get_pretrained_config(model_id) -> str:
+    """Returns the config for a model_id."""
+    return transformers.AutoConfig.from_pretrained(model_id)
+
+
+def task_from_arch(arch: str) -> str:
+    """
+    This function relies on stored information. That information needs to be refresh.
+
+    :param arch: architecture name
+    :return: task
+
+    .. runpython::
+
+        from onnx_diagnostic.torch_models.hub_data import __date__
+        print("last refresh", __date__)
+
+    List of supported architecturs, see
+    :func:`load_architecture_task
+    <onnx_diagnostic.torch_models.hghub.hub_data.load_architecture_task>`.
+    """
+    data = load_architecture_task()
+    assert arch in data, f"Architecture {arch!r} is unknown, last refresh in {__date__}"
+    return data[arch]
+
+
+def task_from_id(model_id: str, pretrained: bool = False) -> str:
     """
     Returns the task attached to a model id.
 
     :param model_id: model id
+    :param pretrained: uses the config
     :return: task
     """
+    if pretrained:
+        config = get_pretrained_config(model_id)
+        try:
+            return config.pipeline_tag
+        except AttributeError:
+            assert config.architectures is not None and len(config.architectures) == 1, (
+                f"Cannot return the task of {model_id!r}, pipeline_tag is not setup, "
+                f"architectures={config.architectures} in config={config}"
+            )
+            return task_from_arch(config.architectures[0])
     return transformers.pipelines.get_task(model_id)
 
 
@@ -18,7 +56,9 @@ def enumerate_model_list(
     task: Optional[str] = None,
     library: Optional[str] = None,
     tags: Optional[Union[str, List[str]]] = None,
+    search: Optional[str] = None,
     dump: Optional[str] = None,
+    filter: Optional[str] = None,
     verbose: int = 0,
 ):
     """
@@ -28,11 +68,21 @@ def enumerate_model_list(
     :param task: see :meth:`huggingface_hub.HfApi.list_models`
     :param tags: see :meth:`huggingface_hub.HfApi.list_models`
     :param library: see :meth:`huggingface_hub.HfApi.list_models`
+    :param search: see :meth:`huggingface_hub.HfApi.list_models`
+    :param filter: see :meth:`huggingface_hub.HfApi.list_models`
     :param dump: dumps the result in this csv file
     :param verbose: show progress
     """
     api = HfApi()
-    models = api.list_models(task=task, library=library, tags=tags)
+    models = api.list_models(
+        task=task,
+        library=library,
+        tags=tags,
+        search=search,
+        full=True,
+        filter=filter,
+        limit=n if n > 0 else None,
+    )
     seen = 0
     found = 0
 
