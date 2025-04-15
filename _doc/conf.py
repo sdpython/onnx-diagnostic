b3896c33f5fb9776e77fd7cd143b06953aa7ff61
xadupre <xadupre@microsoft.com>
2025-04-15 12:20:57 +0200

    oserror'

--- _doc/conf.py
+++ _doc/conf.py
@@ -170,7 +170,7 @@ sphinx_gallery_conf = {
 }
 
 if int(os.environ.get("UNITTEST_GOING", "0")):
-    sphinx_gallery_conf["ignore_pattern"] = ".*((tiny_llm)|(dort)|(draft_mode)).*"
+    sphinx_gallery_conf["ignore_pattern"] = ".*((tiny_llm)|(dort)|(draft_mode)|(hub_codellama.py)).*"
 elif pv.Version(torch.__version__) < pv.Version("2.8"):
     sphinx_gallery_conf["ignore_pattern"] = ".*((_oe_)|(dort)|(draft_mode)).*"
 
