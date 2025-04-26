cd7052ca87a196a3a1396a962d8363c6b31d1993
xadupre <xadupre@microsoft.com>
2025-04-26 10:42:05 +0200

    Add more architectures

--- onnx_diagnostic/__init__.py
+++ onnx_diagnostic/__init__.py
@@ -3,5 +3,5 @@ Investigates onnx models.
 Functions, classes to dig into a model when this one is right, slow, wrong...
 """
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __author__ = "Xavier Dupré"
