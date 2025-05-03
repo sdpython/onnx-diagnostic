47dac083ef778e25b1215175d1c4345fe3746d0d
Xavier Dupré <xadupre@users.noreply.github.com>
2025-05-03 10:45:38 +0200

    upgrade version (#84)

--- onnx_diagnostic/__init__.py
+++ onnx_diagnostic/__init__.py
@@ -3,5 +3,5 @@ Investigates onnx models.
 Functions, classes to dig into a model when this one is right, slow, wrong...
 """
 
-__version__ = "0.4.4"
+__version__ = "0.5.0"
 __author__ = "Xavier Dupré"
