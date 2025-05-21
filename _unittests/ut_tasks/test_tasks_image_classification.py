b8bc033c5ef655a75bb4c733d12eef1234796fe2
xadupre <xadupre@microsoft.com>
2025-05-21 13:34:03 +0200

    change version

--- _unittests/ut_tasks/test_tasks_image_classification.py
+++ _unittests/ut_tasks/test_tasks_image_classification.py
@@ -16,7 +16,7 @@ class TestTasks(ExtTestCase):
         model, inputs, ds = data["model"], data["inputs"], data["dynamic_shapes"]
         model(**inputs)
         model(**data["inputs2"])
-        if not has_transformers("4.51.999"):
+        if not has_transformers("4.52.999"):
             raise unittest.SkipTest("Requires transformers>=4.52")
         with torch_export_patches(patch_transformers=True, verbose=10):
             torch.export.export(
