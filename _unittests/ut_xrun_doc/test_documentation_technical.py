c48e8671bba3e9057c5551b805aef697235bfbcb
xadupre <xadupre@microsoft.com>
2025-06-11 09:32:01 +0200

    dot

--- _unittests/ut_xrun_doc/test_documentation_technical.py
+++ _unittests/ut_xrun_doc/test_documentation_technical.py
@@ -73,7 +73,12 @@ class TestDocumentationTechnical(ExtTestCase):
             if not name.endswith(".py") or not name.startswith("plot_"):
                 continue
             reason = None
-            if not reason and not has_dot and name in {"plot_layer_norm_discrepancies.py"}:
+            if (
+                not reason
+                and not has_dot
+                and name
+                in {"plot_layer_norm_discrepancies.py", "plot_dump_intermediate_results.py"}
+            ):
                 reason = "dot not installed"
 
             if reason:
