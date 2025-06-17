a065e765c018b65603da45962f62e0cf3eb511c7
xadupre <xadupre@microsoft.com>
2025-06-18 00:43:49 +0200

    fix

--- _unittests/ut_export/test_shape_helper.py
+++ _unittests/ut_export/test_shape_helper.py
@@ -1,11 +1,13 @@
 import unittest
 import torch
-from onnx_diagnostic.ext_test_case import ExtTestCase
+from onnx_diagnostic.ext_test_case import ExtTestCase, requires_transformers, requires_torch
 from onnx_diagnostic.export.shape_helper import all_dynamic_shape_from_inputs
 from onnx_diagnostic.torch_models.hghub import get_untrained_model_with_inputs
 
 
 class TestShapeHelper(ExtTestCase):
+    @requires_transformers("4.42")
+    @requires_torch("2.7.99")
     def test_all_dynamic_shape_from_inputs(self):
         ds = all_dynamic_shape_from_inputs((torch.randn((5, 6)), torch.randn((1, 6))))
         self.assertEqual([{0: "d_0_0", 1: "d_0_1"}, {0: "d_1_0", 1: "d_1_1"}], ds)
@@ -20,6 +22,8 @@ class TestShapeHelper(ExtTestCase):
             ds,
         )
 
+    @requires_transformers("4.42")
+    @requires_torch("2.7.99")
     def test_all_dynamic_shape_from_inputs_dynamic_cache(self):
         data = get_untrained_model_with_inputs("arnir0/Tiny-LLM")
         print(self.string_type(data["inputs"], with_shape=True))
