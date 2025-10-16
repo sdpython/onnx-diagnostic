8e07a7a713e27a2c13d585d8031ea5bf6b483ed0
xadupre <xadupre@microsoft.com>
2025-10-16 13:49:27 +0200

    split files

--- _unittests/ut_torch_models/test_validate_whole_models1.py
+++ _unittests/ut_torch_models/test_validate_whole_models1.py
@@ -22,7 +22,7 @@ from onnx_diagnostic.torch_models.validate import (
 from onnx_diagnostic.tasks import supported_tasks
 
 
-class TestValidateWholeModels(ExtTestCase):
+class TestValidateWholeModels1(ExtTestCase):
     def test_a_get_inputs_for_task(self):
         fcts = supported_tasks()
         for task in self.subloop(sorted(fcts)):
@@ -252,30 +252,6 @@ class TestValidateWholeModels(ExtTestCase):
             patch=True,
             rewrite=True,
             optimization="default",
-        )
-        self.assertIsInstance(summary, dict)
-        self.assertIsInstance(data, dict)
-        onnx_filename = data["onnx_filename"]
-        onx = onnx.load(onnx_filename)
-        op_types = set(n.op_type for n in onx.graph.node)
-        self.assertIn("If", op_types)
-
-    @requires_torch("2.9")
-    @hide_stdout()
-    @ignore_warnings(FutureWarning)
-    @requires_transformers("4.55")
-    def test_o_validate_phi35_4k_mini_instruct(self):
-        mid = "microsoft/Phi-3-mini-4k-instruct"
-        summary, data = validate_model(
-            mid,
-            do_run=True,
-            verbose=10,
-            exporter="custom",
-            dump_folder="dump_test/validate_phi35_mini_instruct",
-            inputs2=True,
-            patch=True,
-            rewrite=True,
-            model_options={"rope_scaling": {"rope_type": "dynamic", "factor": 10.0}},
         )
         self.assertIsInstance(summary, dict)
         self.assertIsInstance(data, dict)
---
+++ _unittests/ut_torch_models/test_validate_whole_models2.py
@@ -0,0 +1,40 @@
+import unittest
+import onnx
+from onnx_diagnostic.ext_test_case import (
+    ExtTestCase,
+    hide_stdout,
+    ignore_warnings,
+    requires_torch,
+    requires_transformers,
+)
+from onnx_diagnostic.torch_models.validate import validate_model
+
+
+class TestValidateWholeModels2(ExtTestCase):
+    @requires_torch("2.9")
+    @hide_stdout()
+    @ignore_warnings(FutureWarning)
+    @requires_transformers("4.55")
+    def test_o_validate_phi35_4k_mini_instruct(self):
+        mid = "microsoft/Phi-3-mini-4k-instruct"
+        summary, data = validate_model(
+            mid,
+            do_run=True,
+            verbose=10,
+            exporter="custom",
+            dump_folder="dump_test/validate_phi35_mini_instruct",
+            inputs2=True,
+            patch=True,
+            rewrite=True,
+            model_options={"rope_scaling": {"rope_type": "dynamic", "factor": 10.0}},
+        )
+        self.assertIsInstance(summary, dict)
+        self.assertIsInstance(data, dict)
+        onnx_filename = data["onnx_filename"]
+        onx = onnx.load(onnx_filename)
+        op_types = set(n.op_type for n in onx.graph.node)
+        self.assertIn("If", op_types)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
