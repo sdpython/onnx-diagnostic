e319ead6e1d5250435ddddd994a7f478046d8f78
Xavier Dupré <xadupre@microsoft.com>
2025-10-31 11:10:57 +0100

    split files

---
+++ _unittests/ut_torch_models/test_validate_whole_models3.py
@@ -0,0 +1,60 @@
+import unittest
+from onnx_diagnostic.ext_test_case import (
+    ExtTestCase,
+    hide_stdout,
+    ignore_warnings,
+    requires_torch,
+    requires_transformers,
+)
+from onnx_diagnostic.torch_models.validate import validate_model
+
+
+class TestValidateWholeModels1(ExtTestCase):
+    @requires_torch("2.7")
+    @hide_stdout()
+    @ignore_warnings(FutureWarning)
+    @requires_transformers("4.51")
+    def test_l_validate_model_modelbuilder(self):
+        mid = "microsoft/phi-2"
+        summary, data = validate_model(
+            mid,
+            do_run=True,
+            verbose=10,
+            exporter="modelbuilder",
+            dump_folder="dump_test/validate_model_modelbuilder",
+            patch=False,
+        )
+        self.assertIsInstance(summary, dict)
+        self.assertIsInstance(data, dict)
+        self.assertLess(summary["disc_onnx_ort_run_abs"], 3e-2)
+        onnx_filename = data["onnx_filename"]
+        self.assertExists(onnx_filename)
+
+    @requires_torch("2.7")
+    @hide_stdout()
+    @ignore_warnings(FutureWarning)
+    @requires_transformers("4.51")
+    def test_m_validate_model_vit_model(self):
+        mid = "ydshieh/tiny-random-ViTForImageClassification"
+        summary, data = validate_model(
+            mid,
+            do_run=True,
+            verbose=10,
+            exporter="onnx-dynamo",
+            dump_folder="dump_test/validate_model_vit_model",
+            inputs2=True,
+        )
+        self.assertIsInstance(summary, dict)
+        self.assertIsInstance(data, dict)
+        self.assertLess(summary["disc_onnx_ort_run_abs"], 1e-3)
+        self.assertLess(summary["disc_onnx_ort_run22_abs"], 1e-3)
+        self.assertEqual("dict(pixel_values:A1s2x3x30x30)", summary["run_feeds_inputs"])
+        self.assertEqual("dict(pixel_values:A1s3x3x31x31)", summary["run_feeds_inputs2"])
+        self.assertEqual("#1[A1s2x2]", summary["run_output_inputs"])
+        self.assertEqual("#1[A1s3x2]", summary["run_output_inputs2"])
+        onnx_filename = data["onnx_filename"]
+        self.assertExists(onnx_filename)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
