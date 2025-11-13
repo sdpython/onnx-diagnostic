3cfbc1445650ebc58db81c629fa2e87103ad82d6
Xavier Dupré <xadupre@microsoft.com>
2025-11-13 14:33:07 +0100

    issue with latest transformers

--- _unittests/ut_tasks/test_tasks.py
+++ _unittests/ut_tasks/test_tasks.py
@@ -1,12 +1,7 @@
 import os
 import unittest
 import torch
-from onnx_diagnostic.ext_test_case import (
-    ExtTestCase,
-    hide_stdout,
-    has_transformers,
-    requires_transformers,
-)
+from onnx_diagnostic.ext_test_case import ExtTestCase, hide_stdout, has_transformers
 from onnx_diagnostic.helpers.torch_helper import to_any, torch_deepcopy
 from onnx_diagnostic.torch_models.hghub.model_inputs import get_untrained_model_with_inputs
 from onnx_diagnostic.torch_export_patches import torch_export_patches
@@ -215,50 +210,6 @@ class TestTasks(ExtTestCase):
             torch.export.export(
                 model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
             )
-
-    @hide_stdout()
-    @requires_transformers("4.53.99")
-    def test_feature_extraction_bart_base(self):
-        mid = "facebook/bart-base"
-        data = get_untrained_model_with_inputs(mid, verbose=1, add_second_input=True)
-        self.assertEqual(data["task"], "feature-extraction")
-        self.assertIn((data["size"], data["n_weights"]), [(557681664, 139420416)])
-        model, inputs, ds = data["model"], data["inputs"], data["dynamic_shapes"]
-        model(**torch_deepcopy(inputs))
-        model(**data["inputs2"])
-        with torch_export_patches(patch_transformers=True, verbose=10):
-            torch.export.export(
-                model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
-            )
-
-    @hide_stdout()
-    def test_feature_extraction_tiny_bart(self):
-        mid = "hf-tiny-model-private/tiny-random-PLBartForConditionalGeneration"
-        data = get_untrained_model_with_inputs(mid, verbose=1, add_second_input=True)
-        self.assertEqual(data["task"], "text2text-generation")
-        self.assertIn((data["size"], data["n_weights"]), [(3243392, 810848)])
-        model, inputs, ds = data["model"], data["inputs"], data["dynamic_shapes"]
-        model(**inputs)
-        model(**data["inputs2"])
-        with torch_export_patches(patch_transformers=True, verbose=10):
-            torch.export.export(
-                model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
-            )
-
-    @requires_transformers("4.51.999")
-    @hide_stdout()
-    def test_summarization(self):
-        mid = "facebook/bart-large-cnn"
-        data = get_untrained_model_with_inputs(mid, verbose=1, add_second_input=True)
-        self.assertEqual(data["task"], "summarization")
-        self.assertIn((data["size"], data["n_weights"]), [(1625161728, 406290432)])
-        model, inputs, _ds = data["model"], data["inputs"], data["dynamic_shapes"]
-        model(**inputs)
-        model(**data["inputs2"])
-        # with torch_export_patches(patch_transformers=True, verbose=10):
-        #    torch.export.export(
-        #        model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
-        #    )
 
     @hide_stdout()
     def test_text_classification(self):
---
+++ _unittests/ut_tasks/test_tasks_feature_extraction.py
@@ -0,0 +1,42 @@
+import unittest
+import torch
+from onnx_diagnostic.ext_test_case import ExtTestCase, hide_stdout, requires_transformers
+from onnx_diagnostic.helpers.torch_helper import torch_deepcopy
+from onnx_diagnostic.torch_models.hghub.model_inputs import get_untrained_model_with_inputs
+from onnx_diagnostic.torch_export_patches import torch_export_patches
+from onnx_diagnostic.torch_export_patches.patch_inputs import use_dyn_not_str
+
+
+class TestTasksFeatureExtration(ExtTestCase):
+    @hide_stdout()
+    @requires_transformers("4.53.99")
+    def test_feature_extraction_bart_base(self):
+        mid = "facebook/bart-base"
+        data = get_untrained_model_with_inputs(mid, verbose=1, add_second_input=True)
+        self.assertEqual(data["task"], "feature-extraction")
+        self.assertIn((data["size"], data["n_weights"]), [(557681664, 139420416)])
+        model, inputs, ds = data["model"], data["inputs"], data["dynamic_shapes"]
+        model(**torch_deepcopy(inputs))
+        model(**data["inputs2"])
+        with torch_export_patches(patch_transformers=True, verbose=10):
+            torch.export.export(
+                model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
+            )
+
+    @hide_stdout()
+    def test_feature_extraction_tiny_bart(self):
+        mid = "hf-tiny-model-private/tiny-random-PLBartForConditionalGeneration"
+        data = get_untrained_model_with_inputs(mid, verbose=1, add_second_input=True)
+        self.assertEqual(data["task"], "text2text-generation")
+        self.assertIn((data["size"], data["n_weights"]), [(3243392, 810848)])
+        model, inputs, ds = data["model"], data["inputs"], data["dynamic_shapes"]
+        model(**inputs)
+        model(**data["inputs2"])
+        with torch_export_patches(patch_transformers=True, verbose=10):
+            torch.export.export(
+                model, (), kwargs=inputs, dynamic_shapes=use_dyn_not_str(ds), strict=False
+            )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
