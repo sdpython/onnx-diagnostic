b66b6bb4b7e15d36207fcacbc2d39bfd5e629227
Xavier Dupré <xadupre@microsoft.com>
2026-01-16 17:58:40 +0100

    upgrade version

--- onnx_diagnostic/__init__.py
+++ onnx_diagnostic/__init__.py
@@ -3,5 +3,5 @@ Patches, Investigates onnx models.
 Functions, classes to dig into a model when this one is right, slow, wrong...
 """
 
-__version__ = "0.8.10"
+__version__ = "0.8.11"
 __author__ = "Xavier Dupré"
