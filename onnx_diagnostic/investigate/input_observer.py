324c34bf02e2fcaed788dbc50b624b738ca43fb4
Xavier Dupré <xadupre@microsoft.com>
2026-02-13 16:40:47 +0100

    last try

--- onnx_diagnostic/investigate/input_observer.py
+++ onnx_diagnostic/investigate/input_observer.py
@@ -108,7 +108,7 @@ class InputCandidate:
         kwargs: Optional arguments.
         clone: Clone the inputs before storing them. Some tensors
             may be modified inplace, the original value must be retained.
-        cst_kwargs: Any optional arguments constant over multiple calls.
+        cst_kwargs: Any optional arguments constant over multiple calls,
             int, float, str, bool values must be stored here.
 
     The constructor flattens the received arguments.
